a8ce8d94fca92b063800c1d03e513e645691e8f1
Osama-Elsamny <oelsamny@sfu.ca>
2019-03-21 16:16:57 -0700

    Fixing Exception in guiMain

--- src/main/java/com/colourMe/gui/guiMain.java
+++ src/main/java/com/colourMe/gui/guiMain.java
@@ -5,9 +5,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class guiMain extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("mainPage.css").toExternalForm());
