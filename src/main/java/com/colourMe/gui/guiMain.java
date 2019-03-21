7fdffdac0632e215c8a88c7a00f930390e0adbb6
Osama-Elsamny <oelsamny@sfu.ca>
2019-03-20 20:12:26 -0700

    Added more feature for the game page

--- src/main/java/com/colourMe/gui/guiMain.java
+++ src/main/java/com/colourMe/gui/guiMain.java
@@ -1,53 +1,21 @@
 package com.colourMe.gui;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-//public class Main extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-////        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-////        Scene scene = new Scene(root);
-////        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-////        primaryStage.setTitle("ColourMe");
-////        primaryStage.setScene(scene);
-////        primaryStage.show();
-//        Parent root = FXMLLoader.load(getClass().getResource("grid.fxml"));
-//        Scene scene = new Scene(root);
-//        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//        primaryStage.setTitle("ColourMe");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
 import javafx.application.Application;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class guiMain extends Application {
+    long counter = 0;
     private StackPane createCell(BooleanProperty cellSwitch) {
-
         StackPane cell = new StackPane();
         Canvas cellCanvas = new Canvas(100,100);
         final GraphicsContext graphicsContext = cellCanvas.getGraphicsContext2D();
@@ -55,8 +23,12 @@ public class guiMain extends Application {
         cellCanvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent event) {
+                counter = 0;
                 graphicsContext.beginPath();
                 graphicsContext.moveTo(event.getX(), event.getY());
+                counter++;
+                //System.out.println(event.getX());
+                //System.out.println(event.getY());
 //                graphicsContext.stroke();
             }
         });
@@ -66,6 +38,11 @@ public class guiMain extends Application {
             public void handle(MouseEvent event) {
                 graphicsContext.lineTo(event.getX(), event.getY());
                 graphicsContext.stroke();
+//                System.out.println("------------------------");
+//                counter++;
+//                System.out.println("X " + event.getX());
+//                System.out.println("Y " + event.getY());
+//                System.out.println("------------------------");
 //              graphicsContext.closePath();
 //              graphicsContext.beginPath();
 //              graphicsContext.moveTo(event.getX(), event.getY());
@@ -75,9 +52,10 @@ public class guiMain extends Application {
         cellCanvas.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent event) {
-//              graphicsContext.lineTo(event.getX(), event.getY());
-//              graphicsContext.stroke();
-//              graphicsContext.closePath();
+                graphicsContext.lineTo(event.getX(), event.getY());
+                graphicsContext.stroke();
+                graphicsContext.closePath();
+                System.out.println(counter);
             }
         });
         cell.getChildren().add(cellCanvas);
@@ -119,23 +97,19 @@ public class guiMain extends Application {
     private void initDraw(GraphicsContext gc){
         double canvasWidth = gc.getCanvas().getWidth();
         double canvasHeight = gc.getCanvas().getHeight();
-
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(5);
-        gc.strokeRect(
-                0,              //x of the upper left corner
-                0,              //y of the upper left corner
-                canvasWidth,    //width of the rectangle
-                canvasHeight);  //height of the rectangle
-
+        gc.strokeRect(0, 0, canvasWidth, canvasHeight);
         gc.setStroke(Color.BLUE);
-        gc.setLineWidth(1);
+        gc.setLineWidth(10);
     }
 
     @Override
     public void start(Stage primaryStage) {
         int numCols = 5 ;
         int numRows = 5 ;
+        BorderPane root = new BorderPane();
+        Label welcomeLabel = new Label("ColourMe");
         BooleanProperty[][] switches = new BooleanProperty[numCols][numRows];
         for (int x = 0 ; x < numCols ; x++) {
             for (int y = 0 ; y < numRows ; y++) {
@@ -143,16 +117,38 @@ public class guiMain extends Application {
             }
         }
         GridPane grid = createGrid(switches);
-        StackPane root = new StackPane(grid);
+        AnchorPane leftAnchorPane = new AnchorPane();
+        AnchorPane topAnchorPane = new AnchorPane();
+        //Setting the anchor
+        AnchorPane.setTopAnchor(welcomeLabel, 0.0);
+        AnchorPane.setLeftAnchor(welcomeLabel, 0.0);
+        AnchorPane.setRightAnchor(welcomeLabel, 0.0);
+        AnchorPane.setBottomAnchor(welcomeLabel, 0.0);
+        //add id for the css file
+        welcomeLabel.getStyleClass().add("welcomeLabel");
+        leftAnchorPane.getStyleClass().add("LeftAnchorPane");
+        leftAnchorPane.getStyleClass().add("topAnchorPane");
+        //Height and width for label
+        welcomeLabel.setPrefHeight(55);
+        welcomeLabel.setPrefWidth(602);
+        //Height and width for anchor
+        leftAnchorPane.setPrefWidth(112);
+        leftAnchorPane.setPrefHeight(345);
+        //Height and width for anchor
+        topAnchorPane.setPrefWidth(600);
+        topAnchorPane.setPrefHeight(64);
+        topAnchorPane.getChildren().add(welcomeLabel);
+        root.setTop(topAnchorPane);
+        root.setCenter(grid);
+        root.setRight(leftAnchorPane);
+        BorderPane.setAlignment(topAnchorPane, Pos.CENTER);
+        BorderPane.setAlignment(leftAnchorPane, Pos.CENTER);
         Scene scene = new Scene(root, 600, 600);
         scene.getStylesheets().add(getClass().getResource("grid-with-borders.css").toExternalForm());
+        primaryStage.setTitle("ColourMe");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-
-
-
     public static void main(String[] args) {
         launch(args);
     }
