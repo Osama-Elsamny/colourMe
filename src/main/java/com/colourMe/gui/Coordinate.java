e2813cf9096bbd80851d389a7dadc9ca2c594624
ahad <ahadc@sfu.ca>
2019-03-21 20:20:46 -0700

    Implemented functionality for batching X,Y coordinates

---
+++ src/main/java/com/colourMe/gui/Coordinate.java
@@ -0,0 +1,16 @@
+package com.colourMe.gui;
+
+public class Coordinate {
+    public double x;
+    public double y;
+
+    public Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Coordinate: ({0},{1})", x , y);
+    }
+}
