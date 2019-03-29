00a07cd276f958749418e405da70fe3da3cc4e9b
Osama-Elsamny <oelsamny@sfu.ca>
2019-03-28 20:12:26 -0700

    created at upate funciton for the animation class

--- src/main/java/com/colourMe/gui/lobbyController.java
+++ src/main/java/com/colourMe/gui/lobbyController.java
@@ -5,6 +5,7 @@ import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
