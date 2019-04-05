a297a6708c73f67dbf7f42918b3076b3a02bfeea
ahad <ahadc@sfu.ca>
2019-04-05 14:50:11 -0700

    Refactored logging class

---
+++ src/main/java/com/colourMe/common/util/Log.java
@@ -0,0 +1,28 @@
+package com.colourMe.common.util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Log {
+    private static final String LOG_FORMAT
+            = "[%1$tF %1$tT].%1$tL] [%2$s] [%4$s] %5$s %6$s%n";
+    private static boolean initialized = false;
+    private static Level defaultLevel = Level.INFO;
+
+    public static void initLogging() {
+        if (!initialized) {
+            System.setProperty("java.util.logging.SimpleFormatter.format", LOG_FORMAT);
+            initialized = true;
+        }
+    }
+
+    public static <T> Logger get(T object) {
+        Logger logger = Logger.getLogger(object.getClass().getName());
+        logger.setLevel(defaultLevel);
+        return logger;
+    }
+
+    public static void setDefaultLevel(Level level) {
+        defaultLevel = level;
+    }
+}
--- src/test/java/LogTest.java
+++ src/test/java/LogTest.java
@@ -1,22 +1,22 @@
-import com.colourMe.common.util.L;
+import com.colourMe.common.util.Log;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LogTest {
     @Before
     public void init(){
-        L.initLogging();
+        Log.initLogging();
     }
 
     @Test
     public void verifyLog(){
-        L.log(Level.INFO, this, "verifyLog", "Test message");
+
         String test = "Test message";
-        L.log(Level.INFO, this, "Test message");
-        L.info( this, "Test message");
-        L.warn(this, "Test message");
-        L.severe(this, "Test message");
+        Logger logger = Log.get(this);
+        logger.info(test);
+        logger.warning(test);
+        logger.severe(test);
     }
 }
