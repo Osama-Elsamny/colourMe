aec62645899dfab86aa73de451eeadca7b48045f
Asim Himani <ahimani@sfu.ca>
2019-04-05 00:37:32 -0700

    Add getters and setters for coordinate

--- src/main/java/com/colourMe/common/gameState/Coordinate.java
+++ src/main/java/com/colourMe/common/gameState/Coordinate.java
@@ -9,6 +9,22 @@ public class Coordinate {
         this.y = y;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     @Override
     public String toString() {
         return String.format("Coordinate: (%.3f,%.3f)", x , y);
